e502c59297b48ebef92e80b7533ac99c73b1163a
priya.jayakumar <priya.jayakumar>
2021-12-16 16:43:33 +0530

    release 3.1.4 changes

---
+++ Global.asax.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Bold.Licensing;
+using BoldReports.Web;
+using Newtonsoft.Json;
+using BoldReports.Base.Logger;
+using System.Reflection;
+
+namespace ReportsMVCSamples
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            string License = File.ReadAllText(Server.MapPath("BoldLicense.txt"), Encoding.UTF8);
+            log4net.GlobalContext.Properties["LogPath"] = this.GetAppDataFolderPath();
+            BoldReports.Base.Logger.LogExtension.RegisterLog4NetConfig();
+            BoldLicenseProvider.RegisterLicense(License, bool.Parse(System.Configuration.ConfigurationManager.AppSettings["IsOfflineLicense"]));
+            ReportConfig.DefaultSettings = new ReportSettings()
+            {
+                MapSetting = this.GetMapSettings()
+            }.RegisterExtensions(this.GetDataExtension());
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        private List<string> GetDataExtension()
+        {
+            var extensions = !string.IsNullOrEmpty(System.Configuration.ConfigurationManager.AppSettings["ExtAssemblies"]) ? System.Configuration.ConfigurationManager.AppSettings["ExtAssemblies"] : string.Empty;
+            try
+            {
+                var ExtNames = new List<string>(extensions.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries));
+                List<string> ExtCollections = new List<string>();
+                ExtNames.ForEach(Extension => ExtCollections.Add(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "bin", Extension + ".dll")));
+                return ExtCollections;
+            }
+            catch (Exception ex)
+            {
+                LogExtension.LogError("Failed to Load Data Extension", ex, MethodBase.GetCurrentMethod());
+            }
+            return null;
+        }
+
+        public string GetAppDataFolderPath()
+        {
+            try
+            {
+                return System.IO.Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory );
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private BoldReports.Web.MapSetting GetMapSettings()
+        {
+            try
+            {
+                string basePath = HttpContext.Current.Server.MapPath("~/Scripts");
+                return new MapSetting()
+                {   
+                    ShapePath = basePath + "\\ShapeData\\",
+                    MapShapes = JsonConvert.DeserializeObject<List<MapShape>>(System.IO.File.ReadAllText(basePath + "\\ShapeData\\mapshapes.txt"))
+                };
+            }
+            catch (Exception ex)
+            {
+                LogExtension.LogError("Failed to Load Map Settings", ex, MethodBase.GetCurrentMethod());
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Models/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using BoldReports.Models.ReportViewer;
+
+namespace ReportsMVCSamples.Models
+{
+    public class Globals
+    {
+        public static string basePath = HttpContext.Current.Request.ApplicationPath == "/" ? "/" : HttpContext.Current.Request.ApplicationPath + "/";
+        public static string SERVICE_URL = basePath + "api/ReportViewerWebApi";
+        public static string DESIGNER_SERVICE_URL = basePath + "api/ReportDesignerWebApi";
+        public static bool isPhatomJSExist = File.Exists(HttpContext.Current.Server.MapPath("~/Scripts") + @"\PhantomJS\phantomjs.exe");
+    }
+}
